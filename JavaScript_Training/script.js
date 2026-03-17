1e0bfeff89ef935bf6f67a1a15309a77b8c487ad
bkalva-psi <bkalva@primesoft.net>
2026-03-17 21:43:19 +0530

    First JavaScript code

---
+++ JavaScript_Training/script.js
@@ -0,0 +1,6 @@
+console.log("Welcome to JavaScript World");
+const p1=document.getElementById("p1");
+const button1=document.getElementById("button1");
+button1.addEventListener("click",function(){
+p1.innerHTML="Namaste JavaScript";
+});
